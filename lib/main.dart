16c01dc3154d0e44f4d7c38f2e61a06a6c9c7ef8
Jorge Luis Azevedo Júnior <jorge.luis.eng@gmail.com>
2024-02-25 16:06:33 -0300

    Aula 03 - Adicionando o Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:pay_flow/modules/login/login_page.dart';
 import 'package:pay_flow/modules/splash/splash_page.dart';
 import 'package:pay_flow/shared/themes/app_colors.dart';
@@ -6,13 +10,18 @@ import 'package:pay_flow/shared/themes/app_colors.dart';
 import 'package:pay_flow/modules/login/login_page.dart';
 
 void main() {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const AppWidget());
+
 }
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
